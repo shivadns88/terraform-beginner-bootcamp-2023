f90488ad77eab6f0f341e9bbf189ca63225c884b
Shiva_DNS <shivakumar.s.88@gmail.com>
2023-10-05 12:53:32 +0000

    #37 uploading assets using foreach terraform

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -44,7 +44,18 @@ resource "aws_s3_object" "index_html" {
   }
 }
 
-
+resource "aws_s3_object" "upload_assets" {
+  for_each = fileset(var.assets_path, "*.{jpg,png,gif}")
+  bucket = aws_s3_bucket.s3_bucket.bucket
+  key    = "assets/${each.key}"
+  source = "${var.assets_path}$/{each.key}"
+  etag = filemd5("${var.assets_path}${each.key}")
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+      # Ignore changes to etags
+  }
+}
 
 resource "aws_s3_object" "error_html" {
   bucket = aws_s3_bucket.s3_bucket.bucket
@@ -90,3 +101,5 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
 resource "terraform_data" "content_version" {
   input = var.content_version
 } 
+
+#fileset("${path.root}/public/assets", "*.{jpg,png,gif}")
